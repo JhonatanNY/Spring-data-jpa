fc361f29c7ca8f3126993999dc28aabd0e40c503
JhonatanNY <jhonatanset1989@gmail.com>
2026-04-09 17:04:44 -0500

    "cambios"

--- src/main/java/com/example/springboot/jpa/springboot_jpa/SpringbootJpaApplication.java
+++ src/main/java/com/example/springboot/jpa/springboot_jpa/SpringbootJpaApplication.java
@@ -1,11 +1,14 @@
 package com.example.springboot.jpa.springboot_jpa;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.Scanner;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.example.springboot.jpa.springboot_jpa.entities.Person;
 import com.example.springboot.jpa.springboot_jpa.repositories.PersonRepository;
@@ -23,10 +26,115 @@ public class SpringbootJpaApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 
-		// List<Person> persons =  (List<Person>) repository.findAll();
-		List<Person> persons =  (List<Person>) repository.findByProgrammingLanguage("Java");
+		personalizedQueries();
+	}
+
+	@Transactional(readOnly = true)
+	public void personalizedQueries(){
+
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("=========================== Consulta solo el nombre por el id ===========================");
+		System.out.print("Ingrese el id que desea eliminar: ");
+		Long id = scanner.nextLong();
+
+
+		String name = repository.getNameById(id);
+		System.out.println(name);
+		scanner.close();
+	}
+
+	@Transactional
+	public void delete2() {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Ingrese el id que desea eliminar: ");
+		repository.findAll().forEach(valor -> System.out.println(valor));
+		Long id = scanner.nextLong();
+
+		Optional<Person> optionalPerson = repository.findById(id);
+		optionalPerson.ifPresentOrElse(person -> repository.delete(person), ()-> System.out.println("Lo sentimos no existe la persona con ese id"));
+		repository.deleteById(id);
+		repository.findAll().forEach(valor -> System.out.println(valor));
+
+		scanner.close();
+	}
+
+	@Transactional
+	public void delete() {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Ingrese el id que desea eliminar: ");
+		repository.findAll().forEach(valor -> System.out.println(valor));
+		Long id = scanner.nextLong();
+		repository.deleteById(id);
+		repository.findAll().forEach(valor -> System.out.println(valor));
+
+		scanner.close();
+	}
+
+	@Transactional
+	public void update() {
+
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Ingrese el id de la persona: ");
+		Long id = scanner.nextLong();
+
+		Optional<Person> optionalPerson = repository.findById(id);
+		optionalPerson.ifPresent(person -> {
+			System.out.println(person);
+			System.out.println("Ingrese el lenguaje de programación: ");
+			String programmingLanguage = scanner.next();
+			person.setProgrammingLanguage(programmingLanguage);
+			Person personDb = repository.save(person);
+			System.out.println(personDb);
+		});
+
+		scanner.close();
+	}
+
+	@Transactional
+	public void create() {
+
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Ingrese el nombre: ");
+		String name = scanner.next();
+		System.out.println("Ingrese el apellido: ");
+		String lastname = scanner.next();
+		System.out.println("Ingrese el lenguaje de programación: ");
+		String programmingLanguage = scanner.next();
+		scanner.close();
+
+		Person person = new Person(null, name, lastname, programmingLanguage);
+		Person newPerson = repository.save(person);
+		System.out.println(newPerson);
+
+		repository.findById(newPerson.getId()).ifPresent(p -> System.out.println(p));
+	}
+
+	@Transactional(readOnly = true)
+	public void findOne() {
+
+		// Person person = null;
+		// Optional<Person> optionalPerson = repository.findById(1L);
+		// if(optionalPerson.isPresent()){
+		// person = optionalPerson.get();
+		// }
+		// System.out.println(person);
+
+		repository.findByNameContaining("res").ifPresent(person -> System.out.println(person));
+	}
+
+	@Transactional(readOnly = true)
+	public void list() {
+
+		// List<Person> persons = (List<Person>) repository.findAll();
+		List<Person> persons = (List<Person>) repository.findByprogrammingLanguageAndName("Python", "Pepe");
 
 		persons.stream().forEach(person -> System.out.println(person));
+
+		List<Object[]> personsValues = repository.obtenerPersonData("pepe");
+
+		personsValues.stream().forEach(person -> {
+			System.out.println(person[0] + " es experto en " + person[1]);
+		});
 	}
 
 }
--- src/main/java/com/example/springboot/jpa/springboot_jpa/repositories/PersonRepository.java
+++ src/main/java/com/example/springboot/jpa/springboot_jpa/repositories/PersonRepository.java
@@ -1,12 +1,40 @@
 package com.example.springboot.jpa.springboot_jpa.repositories;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.example.springboot.jpa.springboot_jpa.entities.Person;
 import java.util.List;
+import java.util.Optional;
 
 
 public interface PersonRepository extends CrudRepository<Person, Long> {
+
+    
+
+    @Query("select p.name from Person p where p.id=?1")
+    String getNameById(Long id);
+
+    @Query("select p from Person p where p.id=?1")
+    Optional<Person> findOne(Long id);
+
+    @Query("select p from Person p where p.name=?1")
+    Optional<Person> findOneName(String name);
+
+    Optional<Person> findByNameContaining(String name);
+
+    @Query("select p from Person p where p.name like %?1% ")
+    Optional<Person> findOneLikeName(String name);
     
     List<Person> findByProgrammingLanguage(String programmingLanguage);
+    @Query("select p from Person p where p.programmingLanguage=?1 and p.name=?2")
+    List<Person> buscarByProgrammingLanguage(String programmingLanguage, String name);
+    List<Person> findByprogrammingLanguageAndName(String programmingLanguage, String name);
+    @Query("select p.name, p.programmingLanguage from Person p")
+    List<Object[]> obtenerPersonData();
+
+    @Query("select p.name, p.programmingLanguage from Person p where p.name=?1")
+    List<Object[]> obtenerPersonData(String name);
+
+
 }
